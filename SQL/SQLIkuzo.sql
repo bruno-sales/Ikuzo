34487bf31ecce482b821209976fb3f5c872017d1
bruno-sales <brunosales-rj@hotmail.com>
2018-04-29 14:31:25 -0300

    Sql create table file

---
+++ SQL/SQLIkuzo.sql
@@ -0,0 +1,37 @@
+CREATE TABLE Line (
+    LineId varchar(30) NOT NULL,
+    Description varchar(100) NOT NULL,
+    LastUpdateDate [datetime] NOT NULL ,
+    PRIMARY KEY (LineId)
+); 
+
+CREATE TABLE Bus (
+    BusId varchar(30) NOT NULL,
+    LineId varchar(30) NOT NULL,
+    LastUpdateDate [datetime] NOT NULL ,
+    PRIMARY KEY (BusId),
+    FOREIGN KEY (LineId) REFERENCES Line(LineId)
+); 
+
+CREATE TABLE Gps (
+    GpsGuid [uniqueidentifier] NOT NULL DEFAULT (newid()),
+	[BusId] [varchar](30) NOT NULL,
+	[LineId] [varchar](30) NOT NULL,
+	[Latitude] [decimal](12, 6) NOT NULL,
+	[Longitude] [decimal](12, 6) NOT NULL,
+	[Direction] [int] NOT NULL,
+	[TimeStamp] [datetime] NOT NULL,
+    LastUpdateDate [datetime] NOT NULL 
+    PRIMARY KEY (GpsGuid)
+); 
+
+CREATE TABLE Itinerary (
+    ItineraryGuid [uniqueidentifier] NOT NULL DEFAULT (newid()),    
+	[Latitude] [decimal](12, 6) NOT NULL,
+	[Longitude] [decimal](12, 6) NOT NULL,
+    LineId varchar(30) NOT NULL,
+    Returning Bit NOT NULL,
+    LastUpdateDate [datetime] NOT NULL ,
+    PRIMARY KEY (ItineraryGuid),
+    FOREIGN KEY (LineId) REFERENCES Line(LineId)
+); 
